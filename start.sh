ce2587f6fc5f2aba4131ad6b24dd08defb1d9a99
Henk <hbokhoven@gmail.com>
2014-11-27 23:04:20 +0100

    Updated for fig

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
+
 if [[ -e /firstrun ]]; then
 echo "not first run so skipping initialization"
 else
+echo "Waiting 10 sec. for the DB to come up..."
+sleep 10
 echo "Creating the TYPO3 database..."
-echo "create database TYPO3" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h db -P "$DB_PORT_3306_TCP_PORT"
+echo "create database TYPO3" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
 while [ $? -ne 0 ]; do
 sleep 5
-echo "create database TYPO3" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h db -P "$DB_PORT_3306_TCP_PORT"
-echo "show tables" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h db -P "$DB_PORT_3306_TCP_PORT" TYPO3
+echo "create database TYPO3" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
+echo "show tables" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT" TYPO3
 done
 touch /firstrun
 fi
