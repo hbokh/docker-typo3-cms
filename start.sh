bb0e56e89a227c567f1c85dcc0e4c7113d788c74
Henk <hbokhoven@gmail.com>
2014-11-28 16:38:02 +0100

    Cleanup; FIRST_INSTALL moved to here

--- start.sh
+++ start.sh
@@ -1,18 +1,23 @@
 #! /bin/bash
 
 if [[ -e /firstrun ]]; then
-echo "not first run so skipping initialization"
-else
-echo "Waiting 10 sec. for the DB to come up..."
-sleep 10
-echo "Creating the TYPO3 database..."
-echo "create database TYPO3" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
-while [ $? -ne 0 ]; do
-sleep 5
-echo "create database TYPO3" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
-echo "show tables" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT" TYPO3
-done
-touch /firstrun
+echo "Not first run so skipping initialization"
+  else
+
+    echo "Waiting 10 sec. for the DB to come up..."
+    sleep 10
+    echo "Creating the TYPO3 database..."
+    echo "create database TYPO3" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
+
+    while [ $? -ne 0 ]; do
+        sleep 5
+        echo "create database TYPO3" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
+        echo "show tables" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT" TYPO3
+    done
+
+    touch /var/www/site/htdocs/FIRST_INSTALL
+    touch /firstrun
 fi
+
 service php5-fpm start
 nginx 
