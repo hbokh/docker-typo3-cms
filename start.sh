e7eda487c8c4fc4e8d301d925ddb9aa98878531f
Henk <hbokhoven@gmail.com>
2015-01-03 13:35:12 +0100

    Database in UTF8

--- start.sh
+++ start.sh
@@ -7,11 +7,11 @@ echo "Not first run so skipping initialization"
     echo "Waiting 10 sec. for the DB to come up..."
     sleep 10
     echo "Creating the TYPO3 database..."
-    echo "create database TYPO3" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
+    echo "create database TYPO3 CHARACTER SET utf8 COLLATE utf8_general_ci" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
 
     while [ $? -ne 0 ]; do
         sleep 5
-        echo "create database TYPO3" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
+        echo "create database TYPO3 CHARACTER SET utf8 COLLATE utf8_general_ci" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
         echo "show tables" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT" TYPO3
     done
 
